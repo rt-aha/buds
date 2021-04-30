89149c62307e01bbf15635eec65d2716951d1e29
tsai_ray <silentice1534@gmail.com>
2021-04-30 18:53:25 +0800

    chore: commitlint husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,17 @@
+const type = [
+  'feat',
+  'style',
+  'fix',
+  'chore',
+  'docs',
+  'perf',
+  'refactor',
+  'other'
+]
+
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  reles: {
+    'type-enum': [2, 'always', types]
+  }
 };
