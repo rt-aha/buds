072989b1d2629e4898ba494653178a77c2974924
tsai_ray <silentice1534@gmail.com>
2021-05-02 13:59:52 +0800

    :tada: test: test

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
 const t = 'aBC';
 console.log(t);
+
+// "husky": {
+//   "hooks": {
+//     "commit-msg": "commitlint -e $HUSKY_GIT_PARAMS",
+//     "pre-commit": "lint-staged"
+//   },
+//   "lint-staged": {
+//     "*.js": [
+//       "echo \"pre commit js\""
+//     ],
+//     "*.vue": [
+//       "echo \"pre commit vue\""
+//     ]
+//   }
+// },
