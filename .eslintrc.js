a70fbe3f62b13e81b6869ebfb936d22db7c15930
tsai_ray <silentice1534@gmail.com>
2021-05-02 16:29:24 +0800

    :white_check_mark: test: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'no-restricted-syntax': 'off',
+    'no-param-reassign': 'off',
+    'import/no-self-import': 'off',
+    'max-len': 'off',
+    'import/prefer-default-export': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
--- test.js
+++ test.js
@@ -1,17 +1,2 @@
-const t = 'aBCdfasfdsafs';
+const t = 'fdsafjlasdj';
 console.log(t);
-
-// "husky": {
-//   "hooks": {
-//     "commit-msg": "commitlint -e $HUSKY_GIT_PARAMS",
-//     "pre-commit": "lint-staged"
-//   },
-//   "lint-staged": {
-//     "*.js": [
-//       "echo \"pre commit js\""
-//     ],
-//     "*.vue": [
-//       "echo \"pre commit vue\""
-//     ]
-//   }
-// },
